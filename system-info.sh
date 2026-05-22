7bf95254d0cd7edb7d45b821130b405c7dfd09eb
Rabiyapathan <rabiyabanupathan10@gmail.com>
2026-05-22 17:27:34 +0530

    Added system information automation script

---
+++ system-info.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "System Information"
+echo "-------------------"
+echo "Hostname: $(hostname)"
+echo "Current User: $(whoami)"
+echo "Date: $(date)"
+echo "Kernel Version: $(uname -r)"
